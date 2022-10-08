52aec0a00e2eb112bb87269af2d30f9abd0883ea
praneetwalvekar33 <praneetwalvekar@gmail.com>
2022-10-09 02:35:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const inputField = document.querySelector(".input-field");
+const btnTranslate = document.querySelector(".btn-translate");
+btnTranslate.addEventListener("click",()=>{
+    console.log(inputField.value);    
+})
+console.log(inputField.value);
