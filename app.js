fd27a95a3efcf8aa314041fbad6ac1651350dd12
praneetwalvekar33 <praneetwalvekar@gmail.com>
2022-10-10 02:44:54 +0530

    completed the base function

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const inputField = document.querySelector(".input-field");
+const inputField = document.querySelector(".txt-input");
 const btnTranslate = document.querySelector(".btn-translate");
+const outputField = document.querySelector(".output");
 btnTranslate.addEventListener("click",()=>{
-    console.log(inputField.value);    
-})
-console.log(inputField.value);
+    
+    const url = queryUrl(inputField.value);
+    fetch(url).then(response => response.json())
+    .then(json => {
+        const translatedText = json.contents.translated;
+        outputField.innerText = translatedText;
+    })
+    .catch(errorHandler);
+    
+});
+
+function queryUrl(queryValue){
+    const baseUrl = "https://api.funtranslations.com/translate/minion.json";
+
+    const queryUrlLink = baseUrl + "?text=" + queryValue;
+    
+    return queryUrlLink;
+}
+
+function errorHandler(error){
+    alert("Something is wrong with server! try again after sometime");
+}
